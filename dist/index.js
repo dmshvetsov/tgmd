3c438379b58fd607a15ff34f97a78e5a9667b16a
dmshvetsov <shvetsovdm@gmail.com>
2023-08-22 12:16:55 +0200

    add build

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.transpile = void 0;
+var transpile_1 = require("./transpile");
+Object.defineProperty(exports, "transpile", { enumerable: true, get: function () { return transpile_1.transpile; } });
+//# sourceMappingURL=index.js.map
---
+++ dist/transpile.d.ts
@@ -0,0 +1,1 @@
+export declare function transpile(s: string): string;
